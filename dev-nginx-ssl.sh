1864d4d4d9274da44523039635f2a5834ccc2579
nandomardi23 <fernandomardi1@gmail.com>
2023-02-04 23:35:58 +0700

    test

---
+++ dev-nginx-ssl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear;
+
+echo "install Nginx";
+    if [ -d  /etc/nginx/  ]
+    then
+        echo "nginx sudah ada";
+        nginx -t;
+        git clone https://gitlab.com/roboticpuppies/ssl-certificate.git;
+        mv ~/binar/ssl-certificate /etc/nginx;
+        cp ~/binar/dev.conf /etc/nginx/conf.d;
+        nginx -t;
+        sudo systemctl reload nginx;
+    else
+        echo "Nginx belum ada";
+        sudo apt install nginx -y;
+        sudo systemctl enable nginx;
+        git clone https://gitlab.com/roboticpuppies/ssl-certificate.git;
+        mv ~/binar/ssl-certificate /etc/nginx;
+        cp ~/binar/dev.conf /etc/nginx/conf.d;
+        nginx  -t;
+        sudo systemctl reload nginx;
