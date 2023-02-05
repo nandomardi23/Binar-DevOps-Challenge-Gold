c2d2c3c0183d9f4030966c3cd1faab53cb663b1e
nandomardi23 <fernandomardi1@gmail.com>
2023-02-05 16:21:37 +0700

    test

--- prod-nginx-ssl.sh
+++ prod-nginx-ssl.sh
@@ -8,8 +8,8 @@ echo "install Nginx";
         sudo nginx -t;
         sudo mkdir /etc/nginx/ssl;
         git clone https://gitlab.com/roboticpuppies/ssl-certificate.git;
-        mv ~/ssl-certificate/* /etc/nginx/ssl;
-        mv ~/webserver/prod.conf /etc/nginx/conf.d/;
+        sudo mv ~/ssl-certificate/* /etc/nginx/ssl;
+        sudo mv ~/webserver/prod.conf /etc/nginx/conf.d/;
         sudo nginx -t;
         sudo systemctl reload nginx;
     else
@@ -18,8 +18,8 @@ echo "install Nginx";
         sudo systemctl enable nginx;
         sudo mkdir /etc/nginx/ssl;
         git clone https://gitlab.com/roboticpuppies/ssl-certificate.git;
-        mv  ~/ssl-certificate/* /etc/nginx/ssl/;
-        mv  ~/webserver/prod.conf /etc/nginx/conf.d/;
+        sudo mv  ~/ssl-certificate/* /etc/nginx/ssl/;
+        sudo mv  ~/webserver/prod.conf /etc/nginx/conf.d/;
         sudo nginx  -t;
         sudo systemctl reload nginx;
     fi
